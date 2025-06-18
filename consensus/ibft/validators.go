dfa6a69c4eaa8c197e0bb778fe4514c7f78326f8
vijju122 <viju2043@gmail.com>
2025-06-18 11:24:13 +0530

    aded

---
+++ consensus/ibft/validators.go
@@ -0,0 +1,57 @@
+package ibft
+
+import (
+	// "math"
+
+	"github.com/0xPolygon/polygon-edge/types"
+	"github.com/0xPolygon/polygon-edge/validators"
+)
+
+func CalcMaxFaultyNodes(s validators.Validators) int {
+	// Keeping original calculation but not used in quorum override below
+	return (s.Len() - 1) / 3
+}
+
+type QuorumImplementation func(validators.Validators) int
+
+// LegacyQuorumSize overrides quorum to 2 if 2 or more validators exist,
+// allowing block production with just 2 validators.
+func LegacyQuorumSize(set validators.Validators) int {
+	if set.Len() >= 2 {
+		return 2
+	}
+	return set.Len()
+}
+
+// OptimalQuorumSize overrides quorum to 2 if 2 or more validators exist,
+// similar to LegacyQuorumSize.
+func OptimalQuorumSize(set validators.Validators) int {
+	if set.Len() >= 2 {
+		return 2
+	}
+	return set.Len()
+}
+
+func CalcProposer(
+	validators validators.Validators,
+	round uint64,
+	lastProposer types.Address,
+) validators.Validator {
+	var seed uint64
+
+	if lastProposer == types.ZeroAddress {
+		seed = round
+	} else {
+		offset := int64(0)
+
+		if index := validators.Index(lastProposer); index != -1 {
+			offset = index
+		}
+
+		seed = uint64(offset) + round + 1
+	}
+
+	pick := seed % uint64(validators.Len())
+
+	return validators.At(pick)
+}
